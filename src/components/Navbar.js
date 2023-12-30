c2a1b074f732f92201937791af8e6dcbabc303c4
yashgarg2990 <ygarg423@gmail.com>
2023-12-31 00:32:58 +0530

    removing drop down

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,17 +17,7 @@ export default function Navbar(props){
         <li className="nav-item">
           <a className="nav-link active" href="/About">About</a>
         </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-hrefggle" href="/" role="buthrefn" data-bs-hrefggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu active">
-            <li><a className="dropdown-item" href="/">Action</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
-          </ul>
-        </li>
+        
         
       </ul>
       <form className="d-flex" role="search">
